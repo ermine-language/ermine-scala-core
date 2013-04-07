5c97467d6f7ee45a23b880a361489c221e1ebabb
joshcough <joshcough@gmail.com>
2013-04-07 10:33:17 -0400

    added serializers for Core, still need deserializers. fixed indentation in Core

--- src/main/scala/ermine/syntax/Core.scala
+++ src/main/scala/ermine/syntax/Core.scala
@@ -1,6 +1,6 @@
 package ermine.syntax
 
-import f0._
+import bound._
 
 /**
 data Core a
@@ -16,34 +16,29 @@ data Core a
   | AppDict !(Core a) !(Core a)
   deriving (Eq,Show,Read,Functor,Foldable,Traversable)
  */
-import bound._
-
-trait Branch[+V] {
-  def body: Scope[Int, Core, V]
-}
-case class Labeled[+V](tag: Int, body: Scope[Int, Core, V]) extends Branch[V]
-case class Default[+V](body: Scope[Int, Core, V]) extends Branch[V]
+trait Branch[+V] { def body: Scope[Int, Core, V] }
+  case class Labeled[+V](tag: Int, body: Scope[Int, Core, V]) extends Branch[V]
+  case class Default[+V](body: Scope[Int, Core, V]) extends Branch[V]
 
 trait Core[+V]
-case class Var[+V](v: V) extends Core[V]
-case class Data[+V](tag: Int, fields: List[Core[V]]) extends Core[V]
-case class App[+V](f: Core[V], x: Core[V]) extends Core[V]
-case class Lam[+V](arity: Int, body: Scope[Int, Core, V]) extends Core[V]
-case class Let[+V](bindings: List[Scope[Int, Core, V]], body: Scope[Int, Core, V]) extends Core[V]
-case class Dict[+V](supers: List[Core[V]], slots: List[Scope[Int, Core, V]]) extends Core[V]
-case class LamDict[+V](body: Scope[Unit, Core, V]) extends Core[V]
-case class AppDict[+V](f: Core[V], d: Core[V]) extends Core[V]
+  case class Var[+V](v: V) extends Core[V]
+  case class Data[+V](tag: Int, fields: List[Core[V]]) extends Core[V]
+  case class App[+V](f: Core[V], x: Core[V]) extends Core[V]
+  case class Lam[+V](arity: Int, body: Scope[Int, Core, V]) extends Core[V]
+  case class Let[+V](bindings: List[Scope[Int, Core, V]], body: Scope[Int, Core, V]) extends Core[V]
+  case class Case[+V](c: Core[V], branches: List[Branch[V]]) extends Core[V]
+  case class Dict[+V](supers: List[Core[V]], slots: List[Scope[Int, Core, V]]) extends Core[V]
+  case class LamDict[+V](body: Scope[Unit, Core, V]) extends Core[V]
+  case class AppDict[+V](f: Core[V], d: Core[V]) extends Core[V]
 
 trait HardCore extends Core[Nothing]
-case class Super(i: Int) extends HardCore
-case class Slot(i: Int) extends HardCore
-case class LitInt(i: Int) extends HardCore
-case class LitInt64(l: Long) extends HardCore
-case class LitByte(b: Byte) extends HardCore
-case class LitShort(s: Short) extends HardCore
-case class LitString(s: String) extends HardCore
-case class LitChar(c: Char) extends HardCore
-case class LitFloat(f: Float) extends HardCore
-case class LitDouble(d: Double) extends HardCore
-
-
+  case class Super(i: Int)        extends HardCore
+  case class Slot(i: Int)         extends HardCore
+  case class LitInt(i: Int)       extends HardCore
+  case class LitInt64(l: Long)    extends HardCore
+  case class LitByte(b: Byte)     extends HardCore
+  case class LitShort(s: Short)   extends HardCore
+  case class LitString(s: String) extends HardCore
+  case class LitChar(c: Char)     extends HardCore
+  case class LitFloat(f: Float)   extends HardCore
+  case class LitDouble(d: Double) extends HardCore
---
+++ src/main/scala/ermine/syntax/CoreSerialization.scala
@@ -0,0 +1,72 @@
+package ermine.syntax
+
+import f0._
+import Writers._
+import bound._
+import bound.BoundSerialization._
+
+object CoreSerialization {
+  val hardcoreW: Writer[HardCore, DynamicF] = s10W(
+    intW,
+    intW,
+    intW,
+    longW,
+    byteW,
+    shortW,
+    stringW,
+    intW.cmap((c:Char) => c.toInt),
+    floatW,
+    doubleW
+  )((a,b,c,d,e,f,g,h,i,j) => (hc: HardCore) => hc match {
+    case Super(i)     => a(i)
+    case Slot(i)      => b(i)
+    case LitInt(i)    => c(i)
+    case LitInt64(l)  => d(l)
+    case LitByte(b)   => e(b)
+    case LitShort(s)  => f(s)
+    case LitString(s) => g(s)
+    case LitChar(c)   => h(c)
+    case LitFloat(f)  => i(f)
+    case LitDouble(d) => j(d)
+  }).erase
+
+  def branchW[V](vw: Writer[V, DynamicF]): Writer[Branch[V], DynamicF] = s2W(
+    tuple2W(intW, scopeIntCoreVW(vw)),  // Labeled
+    scopeIntCoreVW(vw)                  // Default
+  )((labelWriter, defaultWriter) => (branch: Branch[V]) => branch match {
+    case Labeled(tag, body) => labelWriter((tag, body))
+    case Default(     body) => defaultWriter(body)
+  }).erase
+
+  def coreW1: Writer1[Core] = new Writer1[Core] {
+    def apply[A](aw: Writer[A, DynamicF]): Writer[Core[A], DynamicF] = coreW(aw)
+  }
+
+  def scopeIntCoreVW[V] (vw: Writer[V, DynamicF]): Writer[Scope[Int,  Core, V], DynamicF] = scopeW(intW, coreW1, vw)
+
+  def coreW[V](vw: Writer[V, DynamicF]): Writer[Core[V], DynamicF] = {
+    val si = scopeIntCoreVW(vw)
+    s9W(
+      vw,                                       // Var
+      tuple2W(intW, repeatW(coreW(vw))),        // Data
+      tuple2W(coreW(vw), coreW(vw)),            // App
+      tuple2W(intW, si),                        // Lam
+      tuple2W(repeatW(si), si),                 // Let
+      tuple2W(coreW(vw), repeatW(branchW(vw))), // Case
+      tuple2W(repeatW(coreW(vw)), repeatW(si)), // Dict
+      scopeW(unitW, coreW1, vw),                // LamDict
+      tuple2W(coreW(vw), coreW(vw))             // AppDict
+    )((a,b,c,d,e,f,g,h,i) => (core: Core[V]) => core match {
+      case Var(v)            => a(v)
+      case Data(t, fs)       => b((t, fs))
+      case App(f, x)         => c((f, x))
+      case Lam(ar, body)     => d((ar, body))
+      case Let(bs, b)        => e((bs, b))
+      case Case(c, bs)       => f((c, bs))
+      case Dict(sups, slots) => g((sups, slots))
+      case LamDict(b)        => h(b)
+      case AppDict(f, d)     => i((f, d))
+    }).erase
+  }
+}
+
