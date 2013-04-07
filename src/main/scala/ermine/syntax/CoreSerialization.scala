0bcd454732c02c07e6a4c21e21a7aa0fdfa96757
joshcough <joshcough@gmail.com>
2013-04-07 10:39:02 -0400

    fixed missing case in core writer.

--- src/main/scala/ermine/syntax/CoreSerialization.scala
+++ src/main/scala/ermine/syntax/CoreSerialization.scala
@@ -38,7 +38,7 @@ object CoreSerialization {
     case Default(     body) => defaultWriter(body)
   }).erase
 
-  def coreW1: Writer1[Core] = new Writer1[Core] {
+  val coreW1: Writer1[Core] = new Writer1[Core] {
     def apply[A](aw: Writer[A, DynamicF]): Writer[Core[A], DynamicF] = coreW(aw)
   }
 
@@ -46,8 +46,9 @@ object CoreSerialization {
 
   def coreW[V](vw: Writer[V, DynamicF]): Writer[Core[V], DynamicF] = {
     val si = scopeIntCoreVW(vw)
-    s9W(
+    s10W(
       vw,                                       // Var
+      hardcoreW,                                // HardCore
       tuple2W(intW, repeatW(coreW(vw))),        // Data
       tuple2W(coreW(vw), coreW(vw)),            // App
       tuple2W(intW, si),                        // Lam
@@ -56,16 +57,17 @@ object CoreSerialization {
       tuple2W(repeatW(coreW(vw)), repeatW(si)), // Dict
       scopeW(unitW, coreW1, vw),                // LamDict
       tuple2W(coreW(vw), coreW(vw))             // AppDict
-    )((a,b,c,d,e,f,g,h,i) => (core: Core[V]) => core match {
+    )((a,b,c,d,e,f,g,h,i,j)  => (core: Core[V]) => core match {
       case Var(v)            => a(v)
-      case Data(t, fs)       => b((t, fs))
-      case App(f, x)         => c((f, x))
-      case Lam(ar, body)     => d((ar, body))
-      case Let(bs, b)        => e((bs, b))
-      case Case(c, bs)       => f((c, bs))
-      case Dict(sups, slots) => g((sups, slots))
-      case LamDict(b)        => h(b)
-      case AppDict(f, d)     => i((f, d))
+      case h:HardCore        => b(h)
+      case Data(t, fs)       => c((t, fs))
+      case App(f, x)         => d((f, x))
+      case Lam(ar, body)     => e((ar, body))
+      case Let(bs, b)        => f((bs, b))
+      case Case(c, bs)       => g((c, bs))
+      case Dict(sups, slots) => h((sups, slots))
+      case LamDict(b)        => i(b)
+      case AppDict(f, d)     => j((f, d))
     }).erase
   }
 }
