2bcd2f427599475e519f33114d5dc9a82596b425
Josh Cough <joshcough@gmail.com>
2013-07-22 14:47:20 -0400

    think i fixed the build problemsm and issue #1

--- build.sbt
+++ build.sbt
@@ -1,25 +1,16 @@
-name := "ermine"
+name := "scala-ermine-core"
 
 resolvers += "Typesafe Sonatype Snapshots" at "http://repo.typesafe.com/typesafe/sonatype-snapshots/"
 
-scalaVersion := "2.10.1"
+scalaVersion := "2.10.2"
 
 libraryDependencies ++= Seq(
-  "org.scalaz" %% "scalaz-core" % "7.0-SNAPSHOT",
-  "clarifi" %% "f0" % "0.2-SNAPSHOT",
-  "bound" %% "bound-core" % "0.2-SNAPSHOT",
-  "bound" %% "bound-scalacheck-binding" % "0.2-SNAPSHOT",
-  "bound" %% "bound-f0-binding" % "0.2-SNAPSHOT",
+  "org.scalaz" %% "scalaz-core" % "7.0.2",
   "org.scalacheck" %% "scalacheck" % "1.10.0" % "test"
 )
 
-crossScalaVersions := Seq("2.9.2", "2.9.3", "2.10.0", "2.10.1")
-
 scalacOptions <++= (scalaVersion) map { sv =>
-  val versionDepOpts =
-    if (sv startsWith "2.9") Seq()
-    else Seq("-feature", "-language:higherKinds", "-language:implicitConversions")
-  Seq("-deprecation", "-unchecked") ++ versionDepOpts
+  Seq("-deprecation", "-unchecked", "-feature", "-language:higherKinds", "-language:implicitConversions")
 }
 
 initialCommands in console := "import scalaz._, Scalaz._"
---
+++ project/Build.scala
@@ -0,0 +1,26 @@
+import sbt._
+import Keys._
+
+object ErmineScalaCoreBuild extends Build {
+
+  lazy val ermine = Project( id = "scala-ermine-core" , base = file(".")).dependsOn(machines, f0, boundCore, boundF0)
+
+  lazy val machines  = fromGithub("runarorama", "scala-machines")
+  lazy val boundCore = fromGithub("runarorama", "scala-bound", subProject = Some("core"))
+  lazy val boundF0   = fromGithub("runarorama", "scala-bound", subProject = Some("bound-f0-binding"))
+  lazy val f0        = fromGithub("joshcough" , "f0")
+
+  // force sbt to get the latest version of a project
+  // 'sbt update' doesn't seem to get the latest even though this says that it should
+  // http://stackoverflow.com/questions/8864317/how-do-i-refresh-updated-git-dependency-artifacts-in-sbt
+  // so instead we have to go to github and get the latest version.
+  def fromGithub(githubUser: String, project: String, subProject: Option[String] = None) = {
+    val commitsUrl = "https://api.github.com/repos/"+githubUser+"/"+project+"/commits?sha=master"
+    val sha = scala.io.Source.fromURL(commitsUrl).takeWhile(_ != ',').mkString.dropWhile(_!=':').drop(2).dropRight(1)
+    val projectUri = uri("https://github.com/"+githubUser+"/"+project+".git#" + sha)
+    subProject match {
+      case None => RootProject(projectUri)
+      case Some(sub) => ProjectRef(projectUri, sub)
+    }
+  }
+}
