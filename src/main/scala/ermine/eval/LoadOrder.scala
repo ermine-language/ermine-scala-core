ce88c3398a7731cc6bbbea622e97eacd4ec47068
joshcough <joshcough@gmail.com>
2013-09-16 12:55:11 -0400

    cleaned up and documented the loader

--- src/main/scala/ermine/eval/LoadOrder.scala
+++ src/main/scala/ermine/eval/LoadOrder.scala
@@ -3,33 +3,62 @@ package ermine.eval
 import scalaz._
 import std.map._, std.set._, std.anyVal._, syntax.semigroup._
 
-
 object LoadOrder {
-  type DepSet[A] = Map[A, Set[A]]
-  type Counts[A] = Map[A, Int]
 
-  case class DepGraph[A](ds: DepSet[A], counts: Counts[A], loadable: Set[A]) {
-    def ++(other: DepGraph[A]) = DepGraph[A](ds |+| other.ds, counts |+| other.counts, loadable ++ other.loadable)
+  /**
+   * @param deps Map from A to the set of A's that depend on that A
+   * @param counts Map from A to the number of unloaded A's that that A depends on
+   */
+  case class DepGraph[A](deps: Map[A, Set[A]], counts: Map[A, Int]) {
 
-    def load(m: A): DepGraph[A] = {
-      val newCounts: Counts[A] = if(ds contains m) ds(m).map(_ -> -1).toMap |+| counts else counts
-      val newLoadableSet: Set[A] = (newCounts.filter{ case (_, i) => i <= 0 }.keySet ++ loadable) - m
-      DepGraph[A](ds - m, newCounts - m, newLoadableSet)
-    }
+    /**
+     * Smash together two dependency graphs by:
+     *   Unioning the Sets in deps.
+     *   Adding the Ints in counts.
+     */
+    def ++(dg: DepGraph[A]) = DepGraph[A](deps |+| dg.deps, counts |+| dg.counts)
 
+    /**
+     * 'Load' an A:
+     *
+     *   For everyone who depends on it, decrement their count in the counts map,
+     *   because they now depend on one fewer modules that need to be loaded.
+     *
+     * @param m
+     * @return
+     */
+    def load(m: A) = DepGraph[A](
+      deps - m,
+      (if(deps contains m) deps(m).map(_ -> -1).toMap |+| counts else counts) - m
+    )
+
+    /**
+     * Calculate the loadable set, which consists anyone who's count in the counts map is zero.
+     * (Because the everything they depended on was loaded).
+     * @return
+     */
+    def loadable: Set[A] = counts.filter{ case (_, i) => i <= 0 }.keySet
+
+    /**
+     * Returns a Stream of Sets of A's in the order that they can be loaded.
+     * The A's in each Set can be loaded in parallel, because they don't depend on each other.
+     */
     def ordered: Stream[Set[A]] = loadable.toList match {
       case Nil => Stream.empty
       case as  => loadable #:: as.foldLeft(this)(_ load _).ordered
     }
   }
 
+  /**
+   * Starting from point a, build a DepGraph from the transitive closure of a's dependencies.
+   * @param a
+   * @param depenencies
+   * @tparam A
+   * @return
+   */
   def getDepGraph[A](a: A, depenencies: A => Set[A]): DepGraph[A] = {
     val depsOfA = depenencies(a).toList
-    val d = DepGraph[A](
-      depsOfA.map(_ -> Set(a)).toMap,
-      Map(a -> depsOfA.size),
-      if(depsOfA.isEmpty) Set(a) else Set()
-    )
-    depsOfA.foldLeft(d){ case (dacc, m) => dacc ++ getDepGraph(m, depenencies) }
+    val dgForA  = DepGraph[A](depsOfA.map(_ -> Set(a)).toMap, Map(a -> depsOfA.size))
+    depsOfA.foldLeft(dgForA){ case (dacc, m) => dacc ++ getDepGraph(m, depenencies) }
   }
 }
